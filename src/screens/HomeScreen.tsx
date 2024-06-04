02cbd4d544f1bc3944ae9a3ab628978631a4c2ce
Toojeronix <manajeronimo7@gmail.com>
2024-06-04 13:10:52 -0300

    Create Navigation and Screen Home

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+    return (
+        <View>
+            <Text className="text-5xl">
+                hola mundo
+            </Text>
+        </View>
+    )
+}
